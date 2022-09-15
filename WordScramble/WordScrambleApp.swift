194ac99ed531d5564130253e58ea6f9790e4c960
JooMin Choi <joomin.choi@clearscore.com>
2022-09-15 13:20:07 +0100

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by JooMin Choi on 15/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
